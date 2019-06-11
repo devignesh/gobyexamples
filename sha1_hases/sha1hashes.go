b9d7403bf8a38be5adf66235c1bd6660db56ac4f
devignesh <vinesh1865@gmail.com>
2019-06-11 23:17:37 +0530

    cryptograpy in go

---
+++ sha1_hases/sha1hashes.go
@@ -0,0 +1,13 @@
+package main
+import "crypto/sha1"
+import "fmt"
+
+func main() {
+	s := "vicky kumar"
+	h := sha1.New()
+	h.Write([]byte(s))
+	bs := h.Sum(nil)
+	fmt.Println(s)
+    fmt.Printf("%x\n", bs)
+
+}
