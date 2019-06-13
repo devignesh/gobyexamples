6f7f20154c520763e8255251af3c1d442c1f62ae
devignesh <vinesh1865@gmail.com>
2019-06-13 19:54:47 +0530

    go conddains url parsing

---
+++ url parsing/url.go
@@ -0,0 +1,19 @@
+package main  
+      
+import (  
+	"io"  
+	"net/http"  
+)  
+  
+  
+func main() {  
+	http.HandleFunc("/company", func(res http.ResponseWriter, req *http.Request) {  
+		displayParameter(res, req)  
+	})  
+	println("Enter the url in browser:  http://localhost:8080/company?name=Tom&age=27")  
+	http.ListenAndServe(":8080", nil)  
+}  
+func displayParameter(res http.ResponseWriter, req *http.Request) {  
+	io.WriteString(res, "name: "+req.FormValue("name"))  
+	io.WriteString(res, "\nage: "+req.FormValue("age"))  
+}  
