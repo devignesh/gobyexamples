8d13e4440bc82203a52da55dd43fc8db2afd1bb4
devignesh <vinesh1865@gmail.com>
2019-08-09 20:23:20 +0530

    basic ex

---
+++ practise/test.go
@@ -0,0 +1,10 @@
+package main  
+import "fmt"  
+func main() {  
+   var i int  
+   var f float64  
+   var b bool  
+   var s string  
+   fmt.Printf("%T %T %T %T\n",i,f,b,s) // Prints type of the variable  
+   fmt.Printf("%v   %v      %v  %q     \n", i, f, b, s) //prints initial value of the variable  
+}  
