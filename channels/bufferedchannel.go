59efcb5dbb369cc5a4e93499094897dd918cd882
devignesh <vinesh1865@gmail.com>
2019-08-30 21:22:20 +0530

    buffered channel in go

---
+++ channels/bufferedchannel.go
@@ -0,0 +1,14 @@
+package main
+
+import (  
+    "fmt"
+)
+
+
+func main() {  
+    ch := make(chan string, 2)
+    ch <- "naveen"
+    ch <- "paul"
+    fmt.Println(<- ch)
+    fmt.Println(<- ch)
+}
