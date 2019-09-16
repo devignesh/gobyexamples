697eecae12fa0f52827feb09ef798202ead2ccfc
devignesh <vinesh1865@gmail.com>
2019-09-16 22:20:27 +0530

    errors

---
+++ errors/cuserrors.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type areaError struct {  
+    err    string  //error description
+    length float64 //length which caused the error
+    width  float64 //width which caused the error
+}
+
+func (e *areaError) Error() string {  
+    return e.err
+}
+
+func (e *areaError) lengthNegative() bool {  
+    return e.length < 0
+}
+
+func (e *areaError) widthNegative() bool {  
+    return e.width < 0
+}
+
+func rectArea(length, width float64) (float64, error) {  
+    err := ""
+    if length < 0 {
+        err += "length is less than zero"
+    }
+    if width < 0 {
+        if err == "" {
+            err = "width is less than zero"
+        } else {
+            err += ", width is less than zero"
+        }
+    }
+    if err != "" {
+        return 0, &areaError{err, length, width}
+    }
+    return length * width, nil
+}
+
+func main() {  
+    length, width := -5.0, -9.0
+    area, err := rectArea(length, width)
+    if err != nil {
+        if err, ok := err.(*areaError); ok {
+            if err.lengthNegative() {
+                fmt.Printf("error: length %0.2f is less than zero\n", err.length)
+
+            }
+            if err.widthNegative() {
+                fmt.Printf("error: width %0.2f is less than zero\n", err.width)
+
+            }
+            return
+        }
+    }
+    fmt.Println("area of rect", area)
+}
