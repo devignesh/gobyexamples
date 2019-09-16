1553c5caa6a167a600959113997f6968b108a26f
devignesh <vinesh1865@gmail.com>
2019-09-16 22:15:59 +0530

    custom errors

---
+++ errors/customerrors1.go
@@ -0,0 +1,36 @@
+package main
+
+import (  
+    "fmt"
+    "math"
+)
+
+type areaError struct {  
+    err    string
+    radius float64
+}
+
+func (e *areaError) Error() string {  
+    return fmt.Sprintf("radius %0.2f: %s", e.radius, e.err)
+}
+
+func circleArea(radius float64) (float64, error) {  
+    if radius < 0 {
+        return 0, &areaError{"radius is negative", radius}
+    }
+    return math.Pi * radius * radius, nil
+}
+
+func main() {  
+    radius := -20.0
+    area, err := circleArea(radius)
+    if err != nil {
+        if err, ok := err.(*areaError); ok {
+            fmt.Printf("Radius %0.2f is less than zero", err.radius)
+            return
+        }
+        fmt.Println(err)
+        return
+    }
+    fmt.Printf("Area of rectangle1 %0.2f", area)
+}
