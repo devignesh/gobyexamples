0e4c1976eb1f678123c62d5320317ba95d4a5de0
devignesh <vinesh1865@gmail.com>
2019-09-22 13:53:38 +0530

    reflection pgm

---
+++ reflect/reflection.go
@@ -0,0 +1,21 @@
+package main
+
+type order struct {  
+    ordId      int
+    customerId int
+}
+
+type employee struct {  
+    name string
+    id int
+    address string
+    salary int
+    country string
+}
+
+func createQuery(q interface{}) string {  
+}
+
+func main() {
+
+}
