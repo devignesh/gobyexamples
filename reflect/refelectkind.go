020a00a626ddc738066e11aa6be6a39d7ce65496
devignesh <vinesh1865@gmail.com>
2019-09-22 13:55:39 +0530

    reflect kinf pgm

---
+++ reflect/refelectkind.go
@@ -0,0 +1,28 @@
+package main
+
+import (  
+    "fmt"
+    "reflect"
+)
+
+type order struct {  
+    ordId      int
+    customerId int
+}
+
+func createQuery(q interface{}) {  
+    t := reflect.TypeOf(q)
+    k := t.Kind()
+    fmt.Println("Type ", t)
+    fmt.Println("Kind ", k)
+
+
+}
+func main() {  
+    o := order{
+        ordId:      456,
+        customerId: 56,
+    }
+    createQuery(o)
+
+}
