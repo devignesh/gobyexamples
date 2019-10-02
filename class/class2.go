2f9110fab8ab1c435f66cd6570838fd23d14023a
devignesh <vinesh1865@gmail.com>
2019-10-02 20:43:06 +0530

    class

---
+++ class/class2.go
@@ -0,0 +1,16 @@
+package main
+
+import (  
+    "fmt"
+)
+
+func simple(a func(a, b int) int) {  
+    fmt.Println(a(60, 7))
+}
+
+func main() {  
+    f := func(a, b int) int {
+        return a + b
+    }
+    simple(f)
+}
