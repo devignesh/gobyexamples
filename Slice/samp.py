50afd84b55b228a67476220e31c4942afe965853
devignesh <vinesh1865@gmail.com>
2020-07-06 15:23:06 +0530

    list comprehesion

---
+++ Slice/samp.py
@@ -0,0 +1,10 @@
+# n = []
+# for i in range(100):
+#     if i % 2 == 0:
+#         n.append(i*i)
+# # print(n)
+# print("n = %s" % n)
+
+n1 = [i*i for i in range(100) if i % 2 == 0 ]
+# n1.append(i*i)
+print(n1)
