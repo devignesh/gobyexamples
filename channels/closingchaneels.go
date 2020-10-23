2570438a80970222efac0dd55d25eb5382047c3a
devignesh <vinesh1865@gmail.com>
2020-10-23 12:26:37 +0530

    updation

--- channels/closingchaneels.go
+++ channels/closingchaneels.go
@@ -1,23 +1,23 @@
 package main
 
-import (  
-    "fmt"
+import (
+	"fmt"
 )
 
-func producer(chnl chan int) {  
-    for i := 0; i < 10; i++ {
-        chnl <- i
-    }
-    close(chnl)
+func producer(chnl chan int) {
+	for i := 0; i < 10; i++ {
+		chnl <- i
+	}
+	close(chnl)
 }
-func main() {  
-    ch := make(chan int)
-    go producer(ch)
-    for {
-        v, ok := <-ch
-        if ok == false {
-            break
-        }
-        fmt.Println("Received ", v, ok)
-    }
+func main() {
+	ch := make(chan int)
+	go producer(ch)
+	for {
+		v, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println("Received ", v, ok)
+	}
 }
