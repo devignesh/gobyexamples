cc63c492a15c589593ccd6dc3a4360ea8155dc79
devignesh <vinesh1865@gmail.com>
2020-10-23 12:22:53 +0530

    updation

--- channels/bufferedchannel1.go
+++ channels/bufferedchannel1.go
@@ -1,24 +1,24 @@
-package main  
+package main
 
-import (    
-    "fmt"  
-    "time"
+import (
+	"fmt"
+	"time"
 )
 
-func write(ch chan int) {  
-    for i := 0; i < 5; i++ {
-        ch <- i
-        fmt.Println("successfully wrote", i, "to ch")
-    }
-    close(ch)
+func write(ch chan int) {
+	for i := 0; i < 5; i++ {
+		ch <- i
+		fmt.Println("successfully wrote", i, "to ch")
+	}
+	close(ch)
 }
-func main() {  
-    ch := make(chan int, 2)
-    go write(ch)
-    time.Sleep(2 * time.Second)
-    for v := range ch {
-        fmt.Println("read value", v,"from ch")
-        time.Sleep(2 * time.Second)
+func main() {
+	ch := make(chan int, 2)
+	go write(ch)
+	time.Sleep(2 * time.Second)
+	for v := range ch {
+		fmt.Println("read value", v, "from ch")
+		time.Sleep(2 * time.Second)
 
-    }
+	}
 }
