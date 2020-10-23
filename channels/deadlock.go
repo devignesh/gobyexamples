80adda3e444396492d3b61c7483933f665a3b231
devignesh <vinesh1865@gmail.com>
2020-10-23 12:23:45 +0530

    updation

--- channels/deadlock.go
+++ channels/deadlock.go
@@ -1,14 +1,14 @@
 package main
 
-import (  
-    "fmt"
+import (
+	"fmt"
 )
 
-func main() {  
-    ch := make(chan string, 2)
-    ch <- "Vicky"
-    ch <- "kumar"
-    ch <- "smazz"
-    fmt.Println(<-ch)
-    fmt.Println(<-ch)
+func main() {
+	ch := make(chan string, 2)
+	ch <- "Vicky"
+	ch <- "kumar"
+	ch <- "smazz"
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
