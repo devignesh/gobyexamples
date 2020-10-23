a32621c4ef7a986a799b07025b86ae3fd9dfc17c
devignesh <vinesh1865@gmail.com>
2020-10-23 12:26:54 +0530

    updation

--- channels/rangeroverchannels.go
+++ channels/rangeroverchannels.go
@@ -1,11 +1,13 @@
 package main
+
 import "fmt"
+
 func main() {
-    queue := make(chan string, 2)
-    queue <- "one"
-    queue <- "two"
-    close(queue)
-    for elem := range queue {
-        fmt.Println(elem)
-    }
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
+	for elem := range queue {
+		fmt.Println(elem)
+	}
 }
