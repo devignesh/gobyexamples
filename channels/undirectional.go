c2cdb54401dcf1e22fc131a45e2c66ac919f20d4
devignesh <vinesh1865@gmail.com>
2020-10-23 12:26:46 +0530

    updation

--- channels/undirectional.go
+++ channels/undirectional.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func sendData(sendch chan<- int) {  
-    sendch <- 10
+func sendData(sendch chan<- int) {
+	sendch <- 10
 }
 
-func main() {  
-    sendch := make(chan<- int)
-    go sendData(sendch)
-    fmt.Println(<-sendch)
+func main() {
+	sendch := make(chan<- int)
+	go sendData(sendch)
+	fmt.Println(<-sendch)
 }
