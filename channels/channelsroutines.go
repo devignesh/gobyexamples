962b9af5666dcb77baa00f0ef7d5bc01058da5c3
devignesh <vinesh1865@gmail.com>
2020-10-28 20:31:14 +0530

    updation

--- channels/channelsroutines.go
+++ channels/channelsroutines.go
@@ -6,12 +6,14 @@ import (
 )
 
 func hello(done chan bool) {
+
 	fmt.Println("hello go routine is going to sleep")
 	time.Sleep(4 * time.Second)
 	fmt.Println("hello go routine awake and going to write to done")
 	done <- true
 }
 func main() {
+
 	done := make(chan bool)
 	fmt.Println("Main going to call hello go goroutine")
 	go hello(done)
