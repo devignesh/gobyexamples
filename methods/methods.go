2d0e03e854325f8e90654b07f9a7c7c65e1b2119
devignesh <vinesh1865@gmail.com>
2020-11-06 20:53:11 +0530

    updation

--- methods/methods.go
+++ methods/methods.go
@@ -1,25 +1,24 @@
 package main
 
-import (  
-    "fmt"
+import (
+	"fmt"
 )
 
-type Employee struct {  
-    name     string
-    salary   int
-    currency string
+type Employee struct {
+	name     string
+	salary   int
+	currency string
 }
 
-
-func (e Employee) displaySalary() {  
-    fmt.Printf("Salary of %s is %s%d", e.name, e.currency, e.salary)
+func (e Employee) displaySalary() {
+	fmt.Printf("Salary of %s is %s%d", e.name, e.currency, e.salary)
 }
 
-func main() {  
-    emp1 := Employee {
-        name:     "vicky",
-        salary:   13000,
-        currency: "$",
-    }
-    emp1.displaySalary() 
+func main() {
+	emp1 := Employee{
+		name:     "vicky",
+		salary:   13000,
+		currency: "$",
+	}
+	emp1.displaySalary()
 }
