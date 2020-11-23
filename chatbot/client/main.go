14fa3d817e30afcb0db6b7c7622e24278fd89c46
devignesh <vinesh1865@gmail.com>
2020-11-23 23:25:58 +0530

    updation

--- chatbot/client/main.go
+++ chatbot/client/main.go
@@ -8,63 +8,65 @@ import (
 	"math/rand"
 	"os"
 	"time"
+
 	"golang.org/x/net/websocket"
-   )
-   type Message struct {
+)
+
+type Message struct {
 	Text string `json:"text"`
-   }
-   var (
+}
+
+var (
 	port = flag.String("port", "9000", "port used for ws connection")
-   )
+)
 
-   func connect() (*websocket.Conn, error) {
+func connect() (*websocket.Conn, error) {
 	return websocket.Dial(fmt.Sprintf("ws://localhost:%s", *port), "", mockedIP())
-   }
+}
 
-
-   func mockedIP() string {
+func mockedIP() string {
 	var arr [4]int
 	for i := 0; i < 4; i++ {
-	  rand.Seed(time.Now().UnixNano())
-	  arr[i] = rand.Intn(256)
+		rand.Seed(time.Now().UnixNano())
+		arr[i] = rand.Intn(256)
 	}
 	return fmt.Sprintf("http://%d.%d.%d.%d", arr[0], arr[1], arr[2], arr[3])
-   }
+}
 
-   func main() {
+func main() {
 	flag.Parse()
-   // connect
+	// connect
 	ws, err := connect()
 	if err != nil {
-	  log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer ws.Close()
-   // receive
+	// receive
 	var m Message
 	go func() {
-	 for {
-	   err := websocket.JSON.Receive(ws, &m)
-	   if err != nil {
-		 fmt.Println("Error receiving message: ", err.Error())
-		 break
-	   }
-	   fmt.Println("Message: ", m)
-	 }
+		for {
+			err := websocket.JSON.Receive(ws, &m)
+			if err != nil {
+				fmt.Println("Error receiving message: ", err.Error())
+				break
+			}
+			fmt.Println("Message: ", m)
+		}
 	}()
-   // send
+	// send
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-	  text := scanner.Text()
-	  if text == "" {
-		continue
-	  }
-	  m := Message{
-		Text: text,
-	  }
-	  err = websocket.JSON.Send(ws, m)
-	  if err != nil {
-		fmt.Println("Error sending message: ", err.Error())
-		break
-	  }
+		text := scanner.Text()
+		if text == "" {
+			continue
+		}
+		m := Message{
+			Text: text,
+		}
+		err = websocket.JSON.Send(ws, m)
+		if err != nil {
+			fmt.Println("Error sending message: ", err.Error())
+			break
+		}
 	}
-   }
+}
