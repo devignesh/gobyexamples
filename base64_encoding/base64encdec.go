f8b49c642ec7de4c5531948dbe2b4b2992394167
devignesh <vinesh1865@gmail.com>
2020-11-23 23:27:43 +0530

    updation

--- base64_encoding/base64encdec.go
+++ base64_encoding/base64encdec.go
@@ -1,15 +1,19 @@
 package main
-import b64 "encoding/base64"
-import "fmt"
+
+import (
+	b64 "encoding/base64"
+	"fmt"
+)
+
 func main() {
-    data := "vicky kumar"
-    sEnc := b64.StdEncoding.EncodeToString([]byte(data))
-    fmt.Println(sEnc)
-    sDec, _ := b64.StdEncoding.DecodeString(sEnc)
-    fmt.Println(string(sDec))
-    fmt.Println()
-    uEnc := b64.URLEncoding.EncodeToString([]byte(data))
-    fmt.Println(uEnc)
-    uDec, _ := b64.URLEncoding.DecodeString(uEnc)
-    fmt.Println(string(uDec))
+	data := "vicky kumar"
+	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
+	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec))
+	fmt.Println()
+	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+	fmt.Println(uEnc)
+	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+	fmt.Println(string(uDec))
 }
