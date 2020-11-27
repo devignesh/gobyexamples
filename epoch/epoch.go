5530cd9a056058adbd536c18c88bf37ec65b6f62
devignesh <vinesh1865@gmail.com>
2020-11-27 13:03:58 +0530

    updation

--- epoch/epoch.go
+++ epoch/epoch.go
@@ -1,16 +1,20 @@
 package main
-import "fmt"
-import "time"
+
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 
 	now := time.Now()
-    secs := now.Unix()
-    nanos := now.UnixNano()
-    fmt.Println(now)
-    millis := nanos / 1000000
-    fmt.Println(secs)
-    fmt.Println(millis)
-    fmt.Println(nanos)
-    fmt.Println(time.Unix(secs, 0))
-    fmt.Println(time.Unix(0, nanos))
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+	millis := nanos / 1000000
+	fmt.Println(secs)
+	fmt.Println(millis)
+	fmt.Println(nanos)
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
 }
