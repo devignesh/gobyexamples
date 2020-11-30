f589b614974ffd35df5e91ed1b98f4a4ec089470
devignesh <vinesh1865@gmail.com>
2020-11-30 22:08:56 +0530

    updation

--- reflect/refelectkind.go
+++ reflect/refelectkind.go
@@ -1,28 +1,27 @@
 package main
 
-import (  
-    "fmt"
-    "reflect"
+import (
+	"fmt"
+	"reflect"
 )
 
-type order struct {  
-    ordId      int
-    customerId int
+type order struct {
+	ordId      int
+	customerId int
 }
 
-func createQuery(q interface{}) {  
-    t := reflect.TypeOf(q)
-    k := t.Kind()
-    fmt.Println("Type ", t)
-    fmt.Println("Kind ", k)
-
+func createQuery(q interface{}) {
+	t := reflect.TypeOf(q)
+	k := t.Kind()
+	fmt.Println("Type ", t)
+	fmt.Println("Kind ", k)
 
 }
-func main() {  
-    o := order{
-        ordId:      456,
-        customerId: 56,
-    }
-    createQuery(o)
+func main() {
+	o := order{
+		ordId:      456,
+		customerId: 56,
+	}
+	createQuery(o)
 
 }
