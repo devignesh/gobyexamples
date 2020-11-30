a62991fd6ffee05c02e0592b5795c553713af8d5
devignesh <vinesh1865@gmail.com>
2020-11-30 22:07:14 +0530

    updating pointer

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,7 +1,9 @@
 package main
+
 import "fmt"
+
 func main() {
-   var a int = 10   
-   fmt.Printf("Address of a variable: %x\n", &a  )
-   fmt.Println("value: ", a)
+	var a int = 10
+	fmt.Printf("Address of a variable: %x\n", &a)
+	fmt.Println("value: ", a)
 }
