c412d342a83908e15930563622b2841bf054e3d2
devignesh <vinesh1865@gmail.com>
2020-12-04 22:38:54 +0530

    updation

--- timers/tickers.go
+++ timers/tickers.go
@@ -1,15 +1,18 @@
 package main
-import "time"
-import "fmt"
+
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-    ticker := time.NewTicker(500 * time.Millisecond)
-    go func() {
-        for t := range ticker.C {
-            fmt.Println("Tick at", t)
-        }
-    }()
-    time.Sleep(1600 * time.Millisecond)
-    ticker.Stop()
-    fmt.Println("Ticker stopped")
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
 }
