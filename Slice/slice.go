c21ec2c6b05433dd1fb9902a2fbb7bc1f078b425
devignesh <vinesh1865@gmail.com>
2021-02-20 21:05:44 +0530

    updation

--- Slice/slice.go
+++ Slice/slice.go
@@ -1,5 +1,7 @@
 package main
+
 import "fmt"
+
 func main() {
 	s := make([]string, 3)
 	fmt.Println("value:", s)
@@ -13,33 +15,32 @@ func main() {
 	fmt.Println("length:", len(s))
 
 	s = append(s, "d")
-    s = append(s, "e", "f")
+	s = append(s, "e", "f")
 	fmt.Println("apd:", s)
-	
 
 	c := make([]string, len(s))
-    copy(c, s)
+	copy(c, s)
 	fmt.Println("cpy:", c)
-	
+
 	l := s[2:5]
 	fmt.Println("sl1:", l)
-	
+
 	l = s[:5]
-    fmt.Println("sl2:", l)
+	fmt.Println("sl2:", l)
 
 	l = s[2:]
-    fmt.Println("sl3:", l)
+	fmt.Println("sl3:", l)
 
 	t := []string{"g", "h", "i"}
-    fmt.Println("dcl:", t)
+	fmt.Println("dcl:", t)
 
 	twoD := make([][]int, 3)
-    for i := 0; i < 3; i++ {
-        innerLen := i + 1
-        twoD[i] = make([]int, innerLen)
-        for j := 0; j < innerLen; j++ {
-            twoD[i][j] = i + j
-        }
-    }
-    fmt.Println("2d: ", twoD)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
 }
